fb55e0f03a290c155ac346508abb4d31a174cb4d
AnoutherLittleEngineThatCould <vvl@mail.ru>
2023-12-19 11:39:30 +0300

    modify App.java(Add lesson_3 task 2 )

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,9 +9,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( inputNumbersSumCheck( 2,11) );
+         numberSign(0) ;
     }
 
     public static boolean inputNumbersSumCheck(int a, int b){
         return a + b >= 10 && a + b <= 20;
+    }
+
+    public static void numberSign(int a){
+        String sign = a >= 0 ? "положительное" : "отрицательное";
+        System.out.println("Передали " + sign +" число");
     }
 }
