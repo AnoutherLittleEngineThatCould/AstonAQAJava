7952e7c0748573db880d2f7ee926d8d946c7c33a
AnoutherLittleEngineThatCould <vvl@mail.ru>
2023-12-19 11:27:43 +0300

    modify App.java(Add lesson_3 task 1 )

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( inputNumbersSumCheck( 2,11) );
+    }
+
+    public static boolean inputNumbersSumCheck(int a, int b){
+        return a + b >= 10 && a + b <= 20;
     }
 }
