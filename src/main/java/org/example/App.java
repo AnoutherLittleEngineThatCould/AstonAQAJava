894ec5c32b0bbfffdb08ea1fc4ff0d80611470e9
AnoutherLittleEngineThatCould <vvl@mail.ru>
2024-01-17 17:42:28 +0300

    modify App.java(Lesson_8)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,30 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        //Task 1:
+        System.out.println("Task 1: ");
+        String[] words = new String[]{"foo", "boo", "bar", "bar", "buzz", "anotherFoo", "foo", "word"
+                , "frog", "boo", "boo"};
+        Map<String, Integer> uniqWordsFrequency = new HashMap<>();
+        for (String s :
+                words) {
+            uniqWordsFrequency.put(s, !uniqWordsFrequency.containsKey(s) ? 1 : uniqWordsFrequency.get(s) + 1);
+        }
+        System.out.println(uniqWordsFrequency);
+        //Task
+        System.out.println("Task 2: ");
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add(Map.of("Ivanov", "+123456789"));
+        phoneBook.add(Map.of("Petrov", "+321654789"));
+        phoneBook.add(Map.of("Ivanov", "+456123789"));
+        phoneBook.add(Map.of("Ivanov", "+987456123"));
+        phoneBook.add(Map.of("Sidorov", "+789456123"));
+        phoneBook.add(Map.of("Petrov", "+654456123"));
+
+        System.out.println(phoneBook.get("Petrov"));
     }
 }
