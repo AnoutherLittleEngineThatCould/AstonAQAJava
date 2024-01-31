83f2646c856ffa22d8a9aa0c6416c178b0b13214
AnoutherLittleEngineThatCould <vvl@mail.ru>
2024-01-31 15:19:24 +0300

    add Lesson_12 junit5

---
+++ src/main/java/org/example/Factorial.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(Factorial.count(3));
+    }
+    public static Long count(int number) {
+        Long res = 1L;
+        if (number == 0) {
+            return res;
+        }
+        else if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                res *= i;
+            }
+            return res;
+        }
+        else {
+            throw new ArithmeticException("Факториа́л — функция, определённая на множестве неотрицательных целых чисел");
+        }
+    }
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,18 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
- * Unit test for simple App.
+ * @author AnoutherLittleEngineThatCould
+ * @link github.com/AnoutherLittleEngineThatCould
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+class FactorialTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  @Test
+    void tes(){
+      Assertions.assertTrue(true);
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
