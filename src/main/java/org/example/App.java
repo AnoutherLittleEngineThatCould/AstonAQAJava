f0994ed5253267e724936a568546c0769229a3c6
AnoutherLittleEngineThatCould <vvl@mail.ru>
2024-01-31 17:21:55 +0300

    add Lesson_12 testNG

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Factorial.count(5));
+        System.out.println(Factorial.count(9));
     }
 }
---
+++ src/test/java/org/example/FactorialTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertThrows;
+
+public class AppTest {
+
+    @DataProvider
+    public Object[][] parseData() {
+        return new Object[][]{
+                {1L, 0},
+                {2L, 2},
+                {479001600L, 12},
+        };
+    }
+
+    @Test(dataProvider = "parseData")
+    void multiTest(Long expected, int input) {
+        assertEquals(expected, Factorial.count(input));
+    }
+
+    @Test
+    void exceptionTesting() {
+
+        assertThrows(
+                () -> Factorial.count(-3)
+        );
+    }
+
+    @Test(expectedExceptions = { ArithmeticException.class }, expectedExceptionsMessageRegExp = ".*Факториа́л.*")
+    public void exceptionTestTwo() {
+        Factorial.count(-10);
+    }
+
+}
