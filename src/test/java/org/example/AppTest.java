c53f20c1b329356575700484021319ed8966078c
AnoutherLittleEngineThatCould <vvl@mail.ru>
2024-01-31 18:20:21 +0300

    modify Test for Lesson_12_junit_5

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,6 +3,8 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,21 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @link github.com/AnoutherLittleEngineThatCould
  */
 class FactorialTest {
-// Почему-то не видит зависимость, в отдельном проекте все работает
-// @ParameterizedTest(name = "{index} test {1}! = {0}")
-//  @CsvSource(value = {"1, 1", "2, 2", "479001600, 12"})
-// void multiTest(Long expected, int input) {
-//    Assertions.assertEquals(expected, Factorial.count(input));
-//  }
 
-    @Test
-    void factorialOneTest() {
-        Assertions.assertEquals(39916800, Factorial.count(11));
-    }
-
-    @Test
-    void factorialTwoTest() {
-        Assertions.assertEquals(39916800, Factorial.count(11));
+    @ParameterizedTest(name = "{index} test {1}! = {0}")
+    @CsvSource(value = {"1, 1", "2, 2", "479001600, 12"})
+    void multiTest(Long expected, int input) {
+        Assertions.assertEquals(expected, Factorial.count(input));
     }
 
     @Test
