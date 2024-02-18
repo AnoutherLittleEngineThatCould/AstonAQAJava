63c910ad604ea94c04bed28a8f06cef87e224282
AnoutherLittleEngineThatCould <vvl@mail.ru>
2024-02-18 19:01:06 +0300

    Lesson_16

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,87 @@
 package org.example;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+public class AppTest {
+    @Test
+    public void checkGetPostmanTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .when()
+                .get("/get")
+                .then().log().body()
+                .statusCode(200)
+                .and().body("headers.x-forwarded-proto", equalTo("https"))
+                .and().body("headers.x-forwarded-port", equalTo("443"))
+                .and().body("headers.host", equalTo("postman-echo.com"))
+                .and().body("headers.x-amzn-trace-id", containsString("Root="))
+                .and().body("headers.accept", equalTo("*/*"))
+                .and().body("headers.user-agent", containsString("Apache-HttpClient"))
+                .and().body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .and().body("url", equalTo("https://postman-echo.com/get"));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void checkPostPostmanTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Some raw text")
+                .when()
+                .post("/post")
+                .then().log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .and().body("headers.x-forwarded-proto", equalTo("https"))
+                .and().body("headers.x-forwarded-port", equalTo("443"))
+                .and().body("headers.host", equalTo("postman-echo.com"))
+                .and().body("headers.content-length", equalTo("13"))
+                .and().body("headers.x-amzn-trace-id", containsString("Root="))
+                .and().body("headers.accept", equalTo("*/*"))
+                .and().body("headers.user-agent", containsString("Apache-HttpClient"))
+                .and().body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .and().body("url", equalTo("https://postman-echo.com/post"));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void checkPutPostmanTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Some raw text")
+                .when()
+                .put("/put")
+                .then().log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .and().body("headers.x-forwarded-proto", equalTo("https"))
+                .and().body("headers.x-forwarded-port", equalTo("443"))
+                .and().body("headers.host", equalTo("postman-echo.com"))
+                .and().body("headers.content-length", equalTo("13"))
+                .and().body("headers.x-amzn-trace-id", containsString("Root="))
+                .and().body("headers.accept", equalTo("*/*"))
+                .and().body("headers.user-agent", containsString("Apache-HttpClient"))
+                .and().body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .and().body("url", equalTo("https://postman-echo.com/put"));
+    }
+
+    @Test
+    public void checkDeletePostmanTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Some raw text")
+                .when()
+                .delete("/delete")
+                .then().log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .and().body("headers.x-forwarded-proto", equalTo("https"))
+                .and().body("headers.x-forwarded-port", equalTo("443"))
+                .and().body("headers.host", equalTo("postman-echo.com"))
+                .and().body("headers.content-length", equalTo("13"))
+                .and().body("headers.x-amzn-trace-id", containsString("Root="))
+                .and().body("headers.accept", equalTo("*/*"))
+                .and().body("headers.user-agent", containsString("Apache-HttpClient"))
+                .and().body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .and().body("url", equalTo("https://postman-echo.com/delete"));
     }
 }
